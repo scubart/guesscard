4bc533d10c5df5d844be6f9b430bf11082c85b87
Bart Dammans <bdammans@gmail.com>
2018-12-30 17:20:12 +0100

    Added chosen color & card reading

--- guesscard.sh
+++ guesscard.sh
@@ -32,7 +32,15 @@ aantal_kaarten=${#Kaart[*]}
 # Willekeurige kaart tonen
 DeKleur=${Kleur[$((RANDOM%aantal_kleuren))]}
 DeKaart=${Kaart[$((RANDOM%aantal_kaarten))]}
-echo ${DeKleur} ${DeKaart}
+
+#echo ${DeKleur} ${DeKaart}
+
+echo -n "Raad de kleur :"
+read GekozenKleur
+
+echo -n "Raad de kaart :"
+read GekozenKaart
+
 
 
 echo That''s all folks!
